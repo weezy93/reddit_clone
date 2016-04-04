1b5c8fd211c63191311cc296fc91b26f2f82cbae
Louisa Reese <louisa.e.reese@gmail.com>
2016-04-04 10:57:55 -0600

    started to add comment funcitonality

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.controller('HomeController', function ($scope) {
     latestID: 1,
     showOnePost: false,
     singlePost: {},
+    commenting: false,
     postArray: [{
       username: '@doge',
       image: 'https://pbs.twimg.com/profile_images/378800000822867536/3f5a00acf72df93528b6bb7cd0a4fd0c.jpeg',
@@ -21,12 +22,13 @@ app.controller('HomeController', function ($scope) {
       username: '@catt',
       image: 'http://cdn1.theodysseyonline.com/files/2015/06/08/6356938644488566691013182599_grumpy-cat.jpg',
       id: 2,
-      body:' Bacon ipsum dolor amet landjaeger chuck meatball chicken hamburger porchetta cow ball tip, turducken capicola rump pork chop boudin short loin salami. Tongue picanha flank meatloaf, strip steak beef leberkas landjaeger shank boudin. Andouille pig kielbasa, tenderloin cupim rump meatball drumstick. Salami pork tongue, turkey biltong shank filet mignon tri-tip corned beef ham.',
+      body:'Morbi interdizzle. My shizz boofron. Maecenizzle nisl. Etiam elit ante, check out this quizzle, rizzle yo mamma, scelerisque fizzle, fizzle. Go to hizzle egizzle neque. Duis felizzle. Sheezy nonummy, nisl you son of a bizzle fringilla cursizzle, fo shizzle mi go to hizzle , sure laorizzle neque enim my shizz crackalackin. Curabitur consequizzle nibh vizzle dawg. Yippiyo shit dolizzle nec libero. Crizzle placerat, ma nizzle vizzle shit lacinia, lorizzle erizzle izzle tortizzle, mofo luctizzle daahng dawg est sizzle est.',
       votes: {
         total: 1,
         upVote: 1,
         downVote: 0,
       },
+      comments: [],
     }],
   };
 
@@ -86,6 +88,10 @@ app.controller('HomeController', function ($scope) {
     $scope.posts.showOnePost = true;
     var index = $scope.posts.postArray.indexOf(postObj);
     return $scope.posts.singlePost = postObj;
+  };
+
+  $scope.action.showComment = function () {
+    $scope.posts.commenting = !$scope.posts.commenting;
   };
 
 });
