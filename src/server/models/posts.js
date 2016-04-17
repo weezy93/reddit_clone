bc754c6ecc6bbce37cacd4596b7901f20fdfa6d0
Louisa Reese <louisa.e.reese@gmail.com>
2016-04-16 22:04:59 -0600

    adding test seeds

--- src/server/models/posts.js
+++ src/server/models/posts.js
@@ -6,7 +6,11 @@ var faker = require('faker');
 var PostSchema = new Schema ({
   username: { type: String, required: true },
   postBody: { type: String, required: true },
-  image: { type: String, required: true }
+  image: { type: String, required: true },
+  votes: { type: Number, default: 0 },
+  postedAt: { type: Date, default: new Date() },
+  comments: { type: Array },
+  showComments: { type: Boolean, default: false }
 });
 
 var Post = mongoose.model('posts', PostSchema);
